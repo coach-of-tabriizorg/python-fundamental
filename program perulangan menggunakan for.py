0e9a15b250e366e5cd30f18889bd59ee18933092
62812 <perdanafiar@gmail.com>
2022-09-22 17:30:15 +0700

    script new

--- program perulangan menggunakan for.py
+++ program perulangan menggunakan for.py
@@ -13,3 +13,17 @@ if bottle_milk_count > 0:
         print("budi membeli 6 botol susu")
 else:
     print("budi tidak jadi membeli botol cucu")
+
+# Perulangan
+
+jumlah_buku = 10
+print("ibu menuruh untuk membaca semua buku")
+
+jumlah_buku_yang_sudah_dibaca= 0
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+for jumlah_buku_yang_sudah_dibaca in range(1, jumlah_buku+1):
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(jumlah_buku_yang_sudah_dibaca)
+
